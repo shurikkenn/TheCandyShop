bdebd602cbc62439c4c08e9a793f5cbc00404d95
shurikkenn <ilogiadis@tutanota.com>
2024-12-23 15:26:22 +0200

    updates to lib file

--- lib.h
+++ lib.h
@@ -2,7 +2,11 @@
 #ifndef LIB.H
 #define LIB_h
 
-//code base
+//viovliothikes
+#include <stdio.h> //gia inout/output
+#include <string.h> //gia tis perigrafes twn proiontwn
+#include <cstddef> //gia na dhlwnoume ta errors
+#include <cstdlib> //gia to exit()
 
 //maximum xarakthres perigrafhs gia kathe proion
 #define MAX_DESC_SIZE 120
@@ -14,10 +18,24 @@ typedef struct {
     char desc[MAX_DESC_SIZE];
     double price;
     unsigned int item_count;
-} product;
+} Product;
 
-int add2inventory(product *p){
-    //
+//to inline benei dioti dhlwnoume sunarthseis se header file
+inline void add2inventory(Product *newproduct, double nPrice, const char *nDesc){
+    //Product newproduct;
+    //arxika diathesima uparxoun 2 proionta sumfwna me thn ekfwnhsh
+    newproduct->item_count = 2;
+    newproduct->price = nPrice;
+    //gia thn perigrafh prwta elenxoume oti den einai kenh
+    if(nDesc != NULL){
+        //h antigrafh ths neas perigrafhs
+        strncpy(newproduct->desc, nDesc, sizeof(newproduct->desc) -1);
+        newproduct->desc[sizeof(newproduct->desc) -1] = '\0';
+    } else {
+        //prepei na ksanadinei o xrhsths upotheto pros to parwn to grafw ws error kai to prograna termatizei
+        fprintf(stderr, "[!] error: %s", "no description given...");
+        exit(1);
+    }
 }
 
 #endif
