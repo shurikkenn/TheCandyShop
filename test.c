00f2b0c6373a5de1a8cdfcac58cd45211c123494
gsghost123 <gsghostgr@gmail.com>
2024-12-24 18:49:41 +0200

    Update test.c

--- test.c
+++ test.c
@@ -1,14 +1,36 @@
 #include <stdio.h>
 
 
-int main(){
-    float price[5]={5.99,7.99,1.99,24.99,10.0};
-    int inStock [5]={30,4,14,24,50};
+int main (){
+    int id = fork();
+    int costumerToShop[2],storeToCostumer[2];//pipes, [0]= read, [1]= write
+    if (id != 0){ //Parent process
+        //ftiaxnoume to catalog
 
-    int i;
-    for (i=0;i<5;i++){
-        printf("item: %d",i," Price: %f",price[i]," in stock: %d\n",inStock[i]);
     }
-    
-    
+    else{ //Child process
+        //Ftiaxnoume tis paragkelies
+        printf("Ti item thelete na agorasete?\n");
+        char itemWanted[];
+        scanf("%s",&itemWanted);
+        write(costumerToShop[1], &itemWanted, sizeof(char) * sizeof(itemWanted));// stelnoume to name tou item pou thelei o pelaths sto parent process
+        close(costumerToShop[1]);
+        int flag;
+        if (read(storeToCostumer[0], &flag, sizeof(int)) < 0){//diabazoume apo to pipe tou parent an yparxei to item pou thelei o costumer
+            return 2;   //error has occured
+        }
+        else {
+            read(storeToCostumer[0], &flag, sizeof(int));
+            }
+        close(storeToCostumer[0]);
+        if (flag==0){
+            printf("To Item pou thelete den yparxei");
+        }
+        else{
+
+        }
+        //an flag = 0 den yparxei, an flag = 1 yparxei
+
+    }
 }
+
